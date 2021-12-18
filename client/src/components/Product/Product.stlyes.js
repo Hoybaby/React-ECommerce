fc0c717c403bdb3e8a9530dd8ded46c593cdd0e8
Hoybaby <mbartek436@gmail.com>
2021-12-18 16:52:09 -0500

    added the generic start for my product components. the product components are going to be very similar to that of the categorie/categorties components

---
+++ client/src/components/Product/Product.stlyes.js
@@ -0,0 +1,18 @@
+
+import styled from "styled-components";
+
+export const Container = styled.div`
+    
+`
+export const t = styled.div`
+    
+`
+export const t = styled.div`
+    
+`
+export const t = styled.div`
+    
+`
+export const t = styled.div`
+    
+`
