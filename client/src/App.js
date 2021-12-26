b87c26f3b74c3669827bf64134f22456536c5a6d
Hoybaby <mbartek436@gmail.com>
2021-12-26 18:45:42 -0500

    slowly composing my new page to hose the products

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-import Home from './pages/Home';
+// import Home from './pages/Home';
 import ProductList from './pages/ProductList';
 
 function App() {
--- client/src/pages/ProductList.jsx
+++ client/src/pages/ProductList.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import {Container} from './ProductList.styles';
+import Navbar from '../components/Navbar/Navbar';
+import Announcement from '../components/Annoucement/Announcement';
 
 const ProductList = () => {
     return (
         <Container>
-            
+            <Navbar/>
+            <Announcement/>
         </Container>
     )
 }
