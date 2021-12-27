79f5c3b3721b2a770d2012459a1451c8715533d5
Hoybaby <mbartek436@gmail.com>
2021-12-27 00:01:53 -0500

    added all the proper tags to the page for my css/styled components to take affect and not have any errors in the console so it can load properly:

--- client/src/pages/Product.jsx
+++ client/src/pages/Product.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import Announcement from '../components/Annoucement/Announcement';
+import Footer from '../components/Footer/Footer';
 import Navbar from '../components/Navbar/Navbar';
 import Newsletter from '../components/Newsletter/Newsletter';
-import { Container } from './styles/Product.styles'
+import { Container, Wrapper, ImgContainer, Image } from './styles/Product.styles'
 
 const Product = () => {
     return (
         <Container>
             <Navbar/>
             <Announcement/>
+            <Wrapper>
+                <ImgContainer>
+                    <Image src="https://i.ibb.co/S6qMxwr/jean.jpg"/>
+                </ImgContainer>
+            </Wrapper>
             <Newsletter/>
             <Footer/>
         </Container>
--- client/src/pages/styles/Product.styles.js
+++ client/src/pages/styles/Product.styles.js
@@ -2,6 +2,12 @@ import styled from "styled-components";
 
 export const Container = styled.div`
 
-    
+
 `;
+
+export const Wrapper = styled.div``;
+
+export const ImgContainer = styled.div``;
+
+export const Image = styled.img``;
 
