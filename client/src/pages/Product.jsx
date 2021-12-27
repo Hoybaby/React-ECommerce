0a8af40dc37db226a991a77010694646eccec962
Hoybaby <mbartek436@gmail.com>
2021-12-27 00:19:43 -0500

    styling is coming along. slowly but surely taking time to tweek

--- client/src/pages/Product.jsx
+++ client/src/pages/Product.jsx
@@ -3,7 +3,7 @@ import Announcement from '../components/Annoucement/Announcement';
 import Footer from '../components/Footer/Footer';
 import Navbar from '../components/Navbar/Navbar';
 import Newsletter from '../components/Newsletter/Newsletter';
-import { Container, Wrapper, ImgContainer, Image } from './styles/Product.styles'
+import { Container, Wrapper, ImgContainer, Image, InfoContainer, Description, Title, Price } from './styles/Product.styles'
 
 const Product = () => {
     return (
@@ -14,6 +14,12 @@ const Product = () => {
                 <ImgContainer>
                     <Image src="https://i.ibb.co/S6qMxwr/jean.jpg"/>
                 </ImgContainer>
+                <InfoContainer>
+                    <Title>Denim Jumpsuit</Title>
+                    <Description>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, 
+                        but also the leap into electronic typesetting, remaining essentially unchanged. </Description>
+                    <Price> $20</Price>
+                </InfoContainer>
             </Wrapper>
             <Newsletter/>
             <Footer/>
--- client/src/pages/styles/Product.styles.js
+++ client/src/pages/styles/Product.styles.js
@@ -5,9 +5,27 @@ export const Container = styled.div`
 
 `;
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+    padding: 50px;
+    display: flex;
+`;
 
-export const ImgContainer = styled.div``;
+export const ImgContainer = styled.div`
+    flex: 1;
+`;
 
-export const Image = styled.img``;
+export const Image = styled.img`
+    width: 100%;
+    height: 90vh;
+    object-fit: cover;
+`;
 
+export const InfoContainer = styled.div`
+    flex: 1;
+`;
+
+export const Title = styled.h1``;
+
+export const Description = styled.p``;
+
+export const Price = styled.span``;
