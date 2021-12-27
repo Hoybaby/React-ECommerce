1f48fa1c86d2b3ef6007212dec09a543ee0aeac9
Hoybaby <mbartek436@gmail.com>
2021-12-26 19:11:37 -0500

    imported the other components and added some styling to the page for the texts. All of this can be done with simple css but I like to do it this way incase I need to use props or anything of that sort.

--- client/src/pages/ProductList.jsx
+++ client/src/pages/ProductList.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
-import {Container} from './ProductList.styles';
+import {Container, Filter, FilterContainer, FilterText, Title} from './ProductList.styles';
 import Navbar from '../components/Navbar/Navbar';
 import Announcement from '../components/Annoucement/Announcement';
+import Products from '../components/Products/Products';
+import Newsletter from '../components/Newsletter/Newsletter';
+import Footer from '../components/Footer/Footer';
 
 const ProductList = () => {
     return (
         <Container>
             <Navbar/>
             <Announcement/>
+            <Title>Dresses</Title>
+            <FilterContainer>
+                <Filter>
+                    <FilterText>Filter Products: </FilterText>
+                </Filter>
+                <Filter>
+                    <FilterText>Sort Products: </FilterText>
+                </Filter>
+            </FilterContainer>
+            <Products/>
+            <Newsletter/>
+            <Footer/>
+
+            
         </Container>
     )
 }
--- client/src/pages/ProductList.styles.js
+++ client/src/pages/ProductList.styles.js
@@ -3,3 +3,22 @@ import styled from "styled-components";
 export const Container = styled.div`
 
 `;
+
+export const FilterContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+
+`;
+
+export const Filter = styled.div`
+    margin: 20px;
+`;
+
+export const Title = styled.h1`
+    margin: 20px;
+`;
+
+export const FilterText = styled.span`
+    font-size: 20px;
+    font-weight: 600;
+`;
