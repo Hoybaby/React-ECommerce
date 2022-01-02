5ac9e2dfc6f3f8ee8cf446287ed3c0d35d5a6c0a
Hoybaby <mbartek436@gmail.com>
2022-01-02 18:35:03 -0500

    added the styling for the singular item/product

--- client/src/pages/styles/Product.styles.js
+++ client/src/pages/styles/Product.styles.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { mobile } from "../../responsive";
 
 export const Container = styled.div`
 
@@ -8,6 +9,7 @@ export const Container = styled.div`
 export const Wrapper = styled.div`
     padding: 50px;
     display: flex;
+    ${mobile({flexDirection: "column", padding: "10px"})};
 `;
 
 export const ImgContainer = styled.div`
@@ -18,11 +20,13 @@ export const Image = styled.img`
     width: 100%;
     height: 90vh;
     object-fit: cover;
+    ${mobile({height: "40%"})}
 `;
 
 export const InfoContainer = styled.div`
     flex: 1;
     padding: 0px 50px;
+    ${mobile({padding: "10px"})};
 `;
 
 export const Title = styled.h1`
@@ -46,6 +50,7 @@ export const FilterContainer = styled.div`
     margin: 30px 0px;
     display: flex;
     justify-content: space-between;
+    ${mobile({width: "100%"})}
 `;
 
 export const Filter = styled.div`
@@ -82,7 +87,8 @@ export const AddContainer = styled.div`
     width: 50%;
     display: flex;
     align-items: center;
-    justify-content: space-between;
+    justify-content: space-between; 
+    ${mobile({width: "100%"})}
 
 `;
 
