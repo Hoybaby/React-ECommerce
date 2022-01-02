9828d30c063174b4d7bb5625cb09d89e3e1c35d9
Hoybaby <mbartek436@gmail.com>
2022-01-02 17:12:18 -0500

    trying to make the application mobile responsive./ tweeking with the navbar on the homepage first

--- client/src/components/Navbar/Navbar.styles.js
+++ client/src/components/Navbar/Navbar.styles.js
@@ -1,10 +1,13 @@
 import styled from 'styled-components';
 
+import { mobile } from '../../responsive';
+
 
 export const Container = styled.div`
     height: 60px;
     /* background-color: black; */
-`
+    ${mobile({backgroundColor: "red"})};
+`;
 
 export const Wrapper = styled.div`
     padding: 10px 20px;
--- client/src/pages/styles/Cart.styles.js
+++ client/src/pages/styles/Cart.styles.js
@@ -162,5 +162,12 @@ export const SummaryItemPrice = styled.span``;
 
 
 
-export const Button = styled.button``;
+export const Button = styled.button`
 
+    width: 100%;
+    padding: 10px;
+    background-color: black;
+    color: white;
+    font-weight: 600;
+
+`
