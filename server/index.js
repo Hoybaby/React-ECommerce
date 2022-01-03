5f98bb1ff098158aba8a66520bdd5e9b9b276683
Hoybaby <mbartek436@gmail.com>
2022-01-03 00:00:37 -0500

    making the foundation for my server for this application i built

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI, )
+.then(
+    () => console.log("Connected to MONGODB database")).catch((err) => console.log(err)
+);
+    
+
+
+app.listen(5000, () => {
+    console.log('Server is running on port 5000');
+});
+
+
