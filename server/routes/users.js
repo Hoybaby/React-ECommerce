296f0198fa2953b53fd40d5f37b9b4062a9e127e
Hoybaby <mbartek436@gmail.com>
2022-01-03 00:18:37 -0500

    trying to test post requests and will do that tomorrow with postman

--- server/routes/users.js
+++ server/routes/users.js
@@ -5,5 +5,12 @@ router.get("/usertest", (req, res) => {
     res.send("User test is successful");
 });
 
+router.post('/userposttest', (req, res) => {
+
+    // going to test this with postman tommorrow morning. last note at 12:18 am 1/3/2022
+    const username = req.body.username;
+    
+    console.log(username)
+})
 
 module.exports = router;
