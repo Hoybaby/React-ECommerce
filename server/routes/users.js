76faaa0b6e924305c8d199ef3b16d520e66b0aa2
Hoybaby <mbartek436@gmail.com>
2022-01-03 16:03:59 -0500

    added the other routes for the different files. just setting them up for now

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,18 +1,20 @@
 const router = require('express').Router();
 
-router.get("/usertest", (req, res) => {
 
-    res.send("User test is successful");
-});
+// WILL DELETE THIS LATER
+// router.get("/usertest", (req, res) => {
 
-router.post('/userposttest', (req, res) => {
+//     res.send("User test is successful");
+// });
 
-    // going to test this with postman tommorrow morning. last note at 12:18 am 1/3/2022
-    // tested and everything works like it should
-    const username = req.body.username;
-    res.send("your username is: " + username);
+// router.post('/userposttest', (req, res) => {
+
+//     // going to test this with postman tommorrow morning. last note at 12:18 am 1/3/2022
+//     // tested and everything works like it should
+//     const username = req.body.username;
+//     res.send("your username is: " + username);
     
-    console.log(username)
-})
+//     console.log(username)
+// })
 
 module.exports = router;
