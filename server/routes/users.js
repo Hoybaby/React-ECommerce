13ebc9cc70e8e282ee052b502edee239c1ff3189
Hoybaby <mbartek436@gmail.com>
2022-01-05 12:16:49 -0500

    was able to update my username with the put method i created. HOWEVER ran into a error for the longeest time because of not importing User like i should have on users.js

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,15 +1,29 @@
 
-const {verifyToken, verifyTokenandAuthorization} = require('./verifyToken');
+const User = require("../models/User");
+const {verifyToken, verifyTokenAndAuthorization} = require('./verifyToken');
 const router = require('express').Router();
 
 
-router.put('/:id', verifyTokenandAuthorization, (req, res) => {
+
+router.put('/:id', verifyTokenAndAuthorization, async(req, res) => {
     // since we assigned req.user in the verify token, we can access it here.
-    if(req.user.id === req.params.id || req.user.isAdmin) {
-        
+    if(req.body.password) {
+        req.body.password = CryptoJS.AES.encrypt(req.body.password, process.env.SECRET_KEY).toString();
     }
 
-    
+    try {
+
+
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+            // this is what we are going to update
+            // take everything inside the req.body and update it
+            $set: req.body
+        }, {new: true});
+        
+        res.status(200).json(updatedUser);
+    } catch(err) {
+        res.status(500).json({message: "Something went wrong in updating the user"});
+    }
 })
 
 module.exports = router;
--- server/routes/verifyToken.js
+++ server/routes/verifyToken.js
@@ -3,21 +3,25 @@ const jwt = require('jsonwebtoken');
 const verifyToken = (req, res, next) => {
 
     const authHeader = req.headers.token;
+
     if(authHeader) {
+
+        const token = authHeader.split(' ')[1];
+
         jwt.verify(token, process.env.JWT_SK, (err, user) => {
-            if(err) res.status(401).json({message: "Invalid token"});
+            if(err) res.status(403).json({message: "Token is not valid"});
             // 
             req.user = user;
             // the next means it will go into the next function which will be user.
             next();
         });
     } else {
-        return res.status(401).json({message: "Not authorized"});
+        return res.status(401).json({message: "You are not Authenticated. Wrong token"});
     }
 
 }
 
-const verifyTokenandAuthorization = (req, res, next) => {
+const verifyTokenAndAuthorization = (req, res, next) => {
     verifyToken(req, res, () => {
         if(req.user.id === req.params.id || req.user.isAdmin) {
             next();
@@ -28,4 +32,4 @@ const verifyTokenandAuthorization = (req, res, next) => {
 };
 
 
-module.exports = {verifyToken, verifyTokenandAuthorization};
+module.exports = {verifyToken, verifyTokenAndAuthorization};
