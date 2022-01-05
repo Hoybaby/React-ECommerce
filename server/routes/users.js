fa6acd536b7643fda3d3fe875d4f6a7b1ba1ac08
Hoybaby <mbartek436@gmail.com>
2022-01-05 10:05:49 -0500

    made a new function in the verifyToken so I can have less code on the user. Insead of manually writing the if else seatement to check if a user isAdmin, i just carry the function over and use a next() to go into the next function

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,8 +1,15 @@
 
-const {verifyToken} = require('./verifyToken');
+const {verifyToken, verifyTokenandAuthorization} = require('./verifyToken');
 const router = require('express').Router();
 
 
-router.put('/:id', verifyToken,)
+router.put('/:id', verifyTokenandAuthorization, (req, res) => {
+    // since we assigned req.user in the verify token, we can access it here.
+    if(req.user.id === req.params.id || req.user.isAdmin) {
+        
+    }
+
+    
+})
 
 module.exports = router;
--- server/routes/verifyToken.js
+++ server/routes/verifyToken.js
@@ -17,4 +17,15 @@ const verifyToken = (req, res, next) => {
 
 }
 
-module.exports = {verifyToken};
+const verifyTokenandAuthorization = (req, res, next) => {
+    verifyToken(req, res, () => {
+        if(req.user.id === req.params.id || req.user.isAdmin) {
+            next();
+        } else {
+            res.status(403).json({message: "Not authorized to do that"});
+        }
+    });
+};
+
+
+module.exports = {verifyToken, verifyTokenandAuthorization};
