727e4c1ba1a09b7bb7938605285268218b6afbba
Hoybaby <mbartek436@gmail.com>
2022-01-11 11:12:23 -0500

    retreived all the products

--- server/routes/product.js
+++ server/routes/product.js
@@ -62,12 +62,24 @@ router.get('/find/:id',  async(req, res) => {
 router.get('/',  async(req, res) => {
 
     const qNew = req.query.new;
-    const qCatefory = req.query.category;
+    const qCategory = req.query.category;
 
     try {
-        const ysers = query 
-        ? await Product.find().sort({_id: -1}).limit(5) 
-        : await Product.find();
+        let products;
+
+        if(qNew) {
+            products = await Product.find().sort({createdAt: -1}).limit(5);
+        } else if (qCategory) {
+            products = await Product.find({categories : {
+                $in: [qCategory],
+            },
+        });
+    } else {
+        products = await Product.find();
+    }
+
+    res.status(200).json(products);
+    
 
     } catch (err) {
         res.status(500).json({message: "Something went wrong in getting the products"});
