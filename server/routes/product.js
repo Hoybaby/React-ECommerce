bf88d20624886ee38be6565ae6796886af0f602f
Hoybaby <mbartek436@gmail.com>
2022-01-10 23:57:54 -0500

    i am uncommenting code that has the same logic with updating a specifc data in my database. isntead of updating the user, it will update the product

--- server/routes/product.js
+++ server/routes/product.js
@@ -17,26 +17,22 @@ router.post("/", verifyTokenAndAdmin, async(req, res) => {
 
 
 
-// router.put('/:id', verifyTokenAndAuthorization, async(req, res) => {
-//     // since we assigned req.user in the verify token, we can access it here.
-//     if(req.body.password) {
-//         req.body.password = CryptoJS.AES.encrypt(req.body.password, process.env.SECRET_KEY).toString();
-//     }
+router.put('/:id', verifyTokenAndAuthorization, async(req, res) => {
+    // since we assigned req.user in the verify token, we can access it here.
 
-//     try {
+    try {
 
-
-//         const updatedUser = await User.findByIdAndUpdate(req.params.id, {
-//             // this is what we are going to update
-//             // take everything inside the req.body and update it
-//             $set: req.body
-//         }, {new: true});
+        const updatedProduct = await Product.findByIdAndUpdate(req.params.id, {
+            // this is what we are going to update
+            // take everything inside the req.body and update it
+            $set: req.body
+        }, {new: true});
         
-//         res.status(200).json(updatedUser);
-//     } catch(err) {
-//         res.status(500).json({message: "Something went wrong in updating the user"});
-//     }
-// })
+        res.status(200).json(updatedProduct);
+    } catch(err) {
+        res.status(500).json({message: "Something went wrong in updating the user"});
+    }
+})
 
 // // delete user
 // router.delete('/:id', verifyTokenAndAuthorization, async(req, res) => {
