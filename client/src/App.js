0f5aa8508d49253bdb88843a2c2cc206578069c1
Hoybaby <mbartek436@gmail.com>
2022-01-19 23:39:45 -0500

    trying ti use react-router dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,27 @@
 
 import './App.css';
-// import Home from './pages/Home';
+import Home from './pages/Home';
 // import ProductList from './pages/ProductList';
 // import Product from './pages/Product';
 // import Register from './pages/Register';
 // import Login from './pages/Login';
 import Cart from './pages/Cart'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    
+} from "react-router-dom";
 
 function App() {
-    return <Cart/>;
+    return 
+    <Router>
+        <Switch>
+            <Route>
+                <Home/>
+            </Route>;
+        </Switch>
+    </Router>
 }
 
 export default App;
