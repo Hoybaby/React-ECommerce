3640dfa915d46c150246a22d2162b86c0601fcb7
Hoybaby <mbartek436@gmail.com>
2022-01-22 17:32:15 -0500

    installed axios to try to call data from my backend to the front. Axios has a lot of beneift

--- client/src/components/Products/Products.jsx
+++ client/src/components/Products/Products.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Container} from './Products.styles';
 import { popularProducts } from "../../data/data";
 import Product from '../Product/Product';
+import { useEffect } from 'react';
 
 
 const Products = ({category, sort, filters}) => {
-    console.log(category, sort, filters)
+
+    const [products, setProducts] = useState([]);
+
+
+    const [filteredProducts, setFilteredProducts] = useState([]);
+
+    useEffect(()=> {
+
+        // when the category changes, we need to filter the products
+    }, [category])
+
+
     return (
         <Container>
             {popularProducts.map((item) => (
